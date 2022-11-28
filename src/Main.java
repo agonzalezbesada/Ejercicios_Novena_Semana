8047ebf14954ffa792bb954365f3854a2fa36660
agonzalezbesada <agonzalezbesada@danielcastelao.org>
2022-11-28 19:37:43 +0100

    Pre arrays

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,9 @@ public class Main {
         perro1.setColor("Negro");
         perro1.setChip(50);
 
+        perro1.setPeso(10);
+        perro1.setAltura(15);
+
         /*
         Al poner los atributos en privado, a este punto no ocurre nada, ya que aunque ahora son privados y no pueden ser accedidos directamente
         desde el main, las funciones que son llamadas si tienen acceso a esos métodos y así pueden ser recibidos igualmente por el main.
@@ -15,8 +18,8 @@ public class Main {
         perro1.setPelo("Largo");
         perro1.setColor("Blanco");
         perro1.setChip(25);
-        
-        System.out.println("El perro 1 tiene "+"el pelo "+perro1.getPelo()+" de color "+perro1.getColor()+" y el número de chip "+perro1.getChip());
+
+        System.out.println(perro1.ladrar(perro1.getPeso(), perro1.getAltura()));
 
 
 
--- src/Perro.java
+++ src/Perro.java
@@ -4,6 +4,9 @@ public class Perro {
     private String color;
     private int chip;
 
+    private int peso;
+    private int altura;
+
     public String getPelo(){
         return pelo;
     }
@@ -31,4 +34,54 @@ public class Perro {
     public void setChip(int chip){
         this.chip = chip;
     }
+
+
+    public int getPeso(){
+        return peso;
+    }
+
+
+    public void setPeso(int peso){
+        this.peso = peso;
+    }
+
+
+    public int getAltura(){
+        return altura;
+    }
+
+
+    public void setAltura(int altura){
+        this. altura = altura;
+    }
+
+
+    public String ladrar(int peso, int altura){
+        String ladrido="";
+
+        if (peso<=10 && altura<=15){
+            ladrido="Guau!";
+
+        }if (peso>10 && altura>15){
+            ladrido="GUAU!!";
+
+        }
+
+        return ladrido;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
